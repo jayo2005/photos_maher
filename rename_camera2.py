1de7cd307c7aeedef6078a2b12fbb7a6ebcf0b4b
jayo2005 <info@dscomputers.ie>
2023-08-15 09:43:18 +0100

    aider: Disable the "Create Folder" button until a base folder has been browsed.
    # Aider chat conversation:
    
    USER: same issue again
    
    ASSISTANT: I see that you're still experiencing the same issue. Let's try a different approach.
    
    Instead of checking if `base_folder_path` is `None` in the `create_folder` method, we can ensure that `browse_folder` method is always called before `create_folder`. We can do this by disabling the "Create Folder" button until a base folder has been browsed.
    
    Here are the changes to make:
    
    ```python
    rename_camera2.py
    <<<<<<< HEAD
        def __init__(self):
            super().__init__()
            global folder_path
    
            self.title("Property Image Processor")
            self.geometry("1000x250")
            self.update_idletasks()
            self.center_window()
    
            self.grid_columnconfigure((0, 1, 2, 3), weight=1)
    
            self.property_name_label = ctk.CTkLabel(self, text="Property Name:")
            self.property_name_label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
            self.property_name_entry = ctk.CTkEntry(self, width=450)
            self.property_name_entry.grid(row=0, column=1, padx=20, pady=10, sticky="w")
            self.browse_button = ctk.CTkButton(self, text="Browse", command=self.browse_folder)
            self.browse_button.grid(row=0, column=2, padx=20, pady=10, sticky="ew")
    
            self.create_folder_button = ctk.CTkButton(self, text="Create Folder", command=self.create_folder)
            self.create_folder_button.grid(row=0, column=3, padx=20, pady=10, sticky="ew")
    =======
        def __init__(self):
            super().__init__()
            global folder_path
    
            self.title("Property Image Processor")
            self.geometry("1000x250")
            self.update_idletasks()
            self.center_window()
    
            self.grid_columnconfigure((0, 1, 2, 3), weight=1)
    
            self.property_name_label = ctk.CTkLabel(self, text="Property Name:")
            self.property_name_label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
            self.property_name_entry = ctk.CTkEntry(self, width=450)
            self.property_name_entry.grid(row=0, column=1, padx=20, pady=10, sticky="w")
            self.browse_button = ctk.CTkButton(self, text="Browse", command=self.browse_folder)
            self.browse_button.grid(row=0, column=2, padx=20, pady=10, sticky="ew")
    
            self.create_folder_button = ctk.CTkButton(self, text="Create Folder", command=self.create_folder, state="disabled")
            self.create_folder_button.grid(row=0, column=3, padx=20, pady=10, sticky="ew")
    >>>>>>> updated
    ```
    
    ```python
    rename_camera2.py
    <<<<<<< HEAD
        def browse_folder(self):
            global base_folder_path
            base_folder_path = filedialog.askdirectory(initialdir="Z:\\")
            self.property_name_entry.delete(0, tk.END)
            self.property_name_entry.insert(0, base_folder_path)
    =======
        def browse_folder(self):
            global base_folder_path
            base_folder_path = filedialog.askdirectory(initialdir="Z:\\")
            self.property_name_entry.delete(0, tk.END)
            self.property_name_entry.insert(0, base_folder_path)
            self.create_folder_button['state'] = "normal"
    >>>>>>> updated
    ```
    
    These changes will disable the "Create Folder" button initially and enable it only after a base folder has been browsed. This will ensure that `base_folder_path` is not `None` when `create_folder` is called.

--- rename_camera2.py
+++ rename_camera2.py
@@ -55,7 +55,7 @@ class App(ctk.CTk):
         self.browse_button = ctk.CTkButton(self, text="Browse", command=self.browse_folder)
         self.browse_button.grid(row=0, column=2, padx=20, pady=10, sticky="ew")
 
-        self.create_folder_button = ctk.CTkButton(self, text="Create Folder", command=self.create_folder)
+        self.create_folder_button = ctk.CTkButton(self, text="Create Folder", command=self.create_folder, state="disabled")
         self.create_folder_button.grid(row=0, column=3, padx=20, pady=10, sticky="ew")
         self.delete_sd_files_button = ctk.CTkButton(self, text="Delete SD Files", command=self.delete_sd_files)
         self.delete_sd_files_button.grid(row=0, column=3, padx=20, pady=10, sticky="ew")
@@ -83,6 +83,7 @@ class App(ctk.CTk):
         base_folder_path = filedialog.askdirectory(initialdir="Z:\\")
         self.property_name_entry.delete(0, tk.END)
         self.property_name_entry.insert(0, base_folder_path)
+        self.create_folder_button['state'] = "normal"
 
     def create_folder(self):
         global base_folder_path, folder_path
